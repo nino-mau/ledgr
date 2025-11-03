0c09e139ebfa9b353ffdc208a95f298037a04f9d
nino-mau <nino.mauuary@gmail.com>
2025-11-03 21:11:04 +0100

    🛠  build(docker): add a docker compose with a frontend dev container

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
 });
