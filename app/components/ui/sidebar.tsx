d84e194c519e23ae608391ee063ffba08dbce739
nino-mau <nino.mauuary@gmail.com>
2025-11-03 21:42:54 +0100

    ✨ feat(sidebar): change height of shadcn sidebar to fill parent container

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -139,7 +139,7 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            'group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full',
+            'group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex size-full',
             className
           )}
           {...props}
@@ -222,7 +222,7 @@ function Sidebar({
           'group-data-[collapsible=offcanvas]:w-0',
           'group-data-[side=right]:rotate-180',
           variant === 'floating' || variant === 'inset'
-            ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]'
+            ? 'h-full group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]'
             : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon)'
         )}
       />
@@ -235,7 +235,7 @@ function Sidebar({
             : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
           // Adjust the padding for floating and inset variants.
           variant === 'floating' || variant === 'inset'
-            ? 'm-(--d-spacing) group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(1))+2px)]'
+            ? 'm-(--d-spacing) !h-auto group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(1))+2px)]'
             : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
           className
         )}
