2524d64709b604187e7d74a92ced6274959ae2a3
nino <nino.mauuary@gmail.com>
2025-11-14 12:15:18 +0100

    :sparkles:feat(sidebar): change width of shadcn sidebar when collapsed

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -29,7 +29,7 @@ const SIDEBAR_COOKIE_NAME = 'sidebar_state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = '16rem';
 const SIDEBAR_WIDTH_MOBILE = '18rem';
-const SIDEBAR_WIDTH_ICON = '3rem';
+const SIDEBAR_WIDTH_ICON = '2rem';
 const SIDEBAR_KEYBOARD_SHORTCUT = 'b';
 
 type SidebarContextProps = {
@@ -235,7 +235,7 @@ function Sidebar({
             : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
           // Adjust the padding for floating and inset variants.
           variant === 'floating' || variant === 'inset'
-            ? 'm-(--d-spacing) !h-auto group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(1))+2px)]'
+            ? 'm-(--d-spacing) !h-auto group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
             : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
           className
         )}
